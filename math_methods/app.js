1ecf74b60875e4ccf5c3bbcb91f8054456994bdd
Hasan Doğan <147431279+hdqn@users.noreply.github.com>
2026-01-14 20:55:26 +0300

    Math sinifi fonksiyonlarina calisildi

---
+++ math_methods/app.js
@@ -0,0 +1,13 @@
+console.log(window);
+
+//Floor Metodu
+//Verdiğimiz değerin noktadan önceki tam sayı degerini verir.Noktadan sonraki kısımı atar. 
+let a = 3.15
+
+console.log(Math.floor(a));
+
+console.log(Math.ceil(a));
+//Ceil Metodu küsürattan sonrasına hiç bakmaksızın bir üst integer sayıya yuvarlar. Round metodu ise en yakın int degerine yuvarlar.
+console.log(Math.round(a));
+
+//Max min degerleri de 
--- string_methods/app.js
+++ string_methods/app.js
@@ -1,6 +1,6 @@
 //String sınıfı metodları 
 
-let baslik = "Javascript Dersleri"
+let baslik = "Modern Javascript Dersleri"
 let tarih = 2026;
 
 //#region CharAt Fonksiyonu
@@ -21,4 +21,49 @@ console.log(sonuc);
 //Verdigimiz degerin index numarasını getirir. Deger yoksa -1 dondurur
 let index = baslik.indexOf("s")
 console.log(index);
+//#endregion
+
+//#region Lastindexof
+let index1 = baslik.lastIndexOf("Ders")
+console.log(index1 + ". index");
+
+//#endregion
+
+//#region ToUpperCase
+//Tüm alfabetik degerleri buyuk harfe donusturur.
+//#endregion
+
+//toLowerCase tum harfleri kucuk harfe cevirir.
+
+
+//#region Trim
+//En cok kullanacagimiz islemlerden bir tanesi
+//Sagdaki ve soldaki bosluklari silip ham veriyi elde etmemizi saglar.
+let deneme = "   Bu bir deneme yazisidir   "
+console.log(deneme);
+console.log(deneme.trim());
+
+//#endregion
+
+//#region Slice-Substring
+//Bu metod verilen stringi dilimlemek icin kullanilir.
+let ornek = "lorem ipsum dolor sit"
+console.log(ornek);
+console.log(ornek.slice(6,11));
+
+console.log(ornek);
+console.log(ornek.substring(0,6));
+//#endregion
+
+//#region replace 
+//Bir kelimeyi degistirmek icin kullaniyoruz
+console.log(ornek);
+console.log(ornek.replace("ipsum","ipsumus"));
+//#endregion
+
+//#region Split
+//Verdigimiz aralıga gore stringi dilimleyip dizi haline getirir
+    console.log(baslik);
+    let dizi = baslik.split(" ");
+    console.log(dizi);
 //#endregion
