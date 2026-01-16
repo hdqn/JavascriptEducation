138f1a3d64411ea9803a66a9bb990328ab877066
Hasan Doğan <147431279+hdqn@users.noreply.github.com>
2026-01-16 14:10:59 +0300

    Math sinifi metodlari ustune calisma yapildi

--- ikinciders/app.js
+++
@@ -1,9 +0,0 @@
-let sonuc;
-sonuc = document.all;
-
-sonuc = document.all[6]
-sonuc = document.all.lenght;
-
-
-
-console.log(sonuc);
--- math_methods/app.js
+++ math_methods/app.js
@@ -10,4 +10,30 @@ console.log(Math.ceil(a));
 //Ceil Metodu küsürattan sonrasına hiç bakmaksızın bir üst integer sayıya yuvarlar. Round metodu ise en yakın int degerine yuvarlar.
 console.log(Math.round(a));
 
-//Max min degerleri de 
+//Max min degerleri de max ve min fonksiyonları ile buluruz.
+console.log(Math.max(1,2,3,4,50));
+
+console.log(Math.min(-1,2,3,4,-23));
+
+
+//Abs Mutlak değer almak icin kullanilir.
+
+console.log(Math.abs(-12));
+
+
+//sqrt fonksiyonu bir fonksiyonun karekökünü almak icin kullanilir.
+
+
+console.log(Math.sqrt(169));
+
+//Üslü sayı almak icin pow
+
+console.log(Math.pow(5,3));
+
+//Random metodu 
+
+console.log(Math.random()); // Burda random fonksiyonu sayesinde 0-1 arasında rasgele bir değer döner.
+
+//Eğer bu değeri 100 ile çarparsak 0-100 arasında değer döner. Ama yine de tam sayı değildir bu yüzden belli metodlarla virgülden sonraki değeri atarız.
+
+console.log(Math.floor(Math.random()*100));
